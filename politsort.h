0499ec7c0a31fc59410065a3e7dca472e15f39c0
Alexander Zabolotskiy <brainexploded@gmail.com>
2025-09-05 12:40:26 +0300

    header fix

--- politsort.h
+++ politsort.h
@@ -4,5 +4,11 @@
 #include <inttypes.h>
 
 void woke_sort(int64_t *arr, const int64_t len);
+void dei_sort(int64_t *arr, const int64_t len);
+void putin_sort(int64_t *arr, int64_t *len);
+void maga_sort(int64_t *arr, const int64_t len);
+void fasc_sort(int64_t *arr, int64_t *len);
+void commie_sort(int64_t *arr, const int64_t len);
+void socialist_sort(int64_t *arr, const int64_t len);
 
 #endif
